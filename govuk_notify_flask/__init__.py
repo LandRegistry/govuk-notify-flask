2e806bed299dc855084f869c8e803c85c2c3a290
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2016-11-08 16:38:14 +0000

    csrf protection

--- govuk_notify_flask/__init__.py
+++ govuk_notify_flask/__init__.py
@@ -1,6 +1,7 @@
 # flake8: noqa
 from flask import Flask
 from flask_compress import Compress
+from flask_wtf.csrf import CsrfProtect
 
 app = Flask(__name__)
 
@@ -9,5 +10,8 @@ app.config.from_pyfile('config.py')
 
 # Flask Compress
 Compress(app)
+
+# CSRF Protection
+CsrfProtect(app)
 
 import govuk_notify_flask.views
