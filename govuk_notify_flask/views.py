861f4e03251fb48b0d17a301b5ae83c6b7b3c92c
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2016-11-15 10:33:55 +0000

    update client to 3.0.0, better error handling

--- govuk_notify_flask/views.py
+++ govuk_notify_flask/views.py
@@ -2,19 +2,24 @@ from govuk_notify_flask import app
 from flask import render_template, flash
 from govuk_notify_flask.forms import EmailForm
 from notifications_python_client.notifications import NotificationsAPIClient
+from notifications_python_client.errors import HTTPError
 
-notifications_client = NotificationsAPIClient(api_key=app.config['NOTIFY_API_KEY'])
+notifications_client = NotificationsAPIClient(app.config['NOTIFY_API_KEY'])
 
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     email_form = EmailForm()
     if email_form.validate_on_submit():
-        notifications_client.send_email_notification(
-            email_form.email_address.data,
-            email_form.template.data
-        )
-        flash('Email sent')
+        try:
+            notifications_client.send_email_notification(
+                email_form.email_address.data,
+                email_form.template.data
+            )
+            flash('Email sent')
+        except HTTPError as e:
+            raise e
+
         return render_template(
             'index.html',
             form=email_form
