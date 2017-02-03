0902f7caf513d558cce501c28a46ee83a604db05
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2017-02-03 19:42:21 +0000

    remove default key

--- govuk_notify_flask/config.py
+++ govuk_notify_flask/config.py
@@ -1,5 +1,4 @@
 import os
 
 SECRET_KEY = os.getenv('SECRET_KEY', '13*#iT@1MT1!Satg')
-NOTIFY_API_KEY = os.getenv('NOTIFY_API_KEY',
-                           'test-92f7f929-38ba-4425-91a2-86e17dfb7d5a-8e4bb9ee-7c9e-4c8c-b84c-720b9c1b7a41')
+NOTIFY_API_KEY = os.getenv('NOTIFY_API_KEY')
