1010f7513a515673e3b41f67b6c7ca29ef0f78e0
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2017-02-06 21:27:47 +0000

    remove unused forms

--- govuk_notify_flask/forms.py
+++ govuk_notify_flask/forms.py
@@ -1,28 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, PasswordField
-from wtforms.validators import DataRequired, Email, EqualTo, Length
+from wtforms.validators import DataRequired, Email, EqualTo
 
-EMAILS = [('e389a7f6-fc65-4b32-9d85-ed067e726735', 'Hello World'),
-          ('1e6a28c8-3555-4e10-897d-8d19ad058598', 'Reset Password')]
-
-TEXTS = [('e1d9690c-2f40-4de0-8417-087f389b3e3f', 'Hello World')]
-
-
-class EmailForm(FlaskForm):
-    email_address = StringField('Email Address', validators=[DataRequired(), Email()])
-    template = SelectField('Template', choices=EMAILS, validators=[DataRequired()])
-
-
-class TextForm(FlaskForm):
-    mobile_number = StringField('Mobile Number', validators=[DataRequired()])
-    template = SelectField('Template', choices=TEXTS, validators=[DataRequired()])
+TITLES = [('', 'Please select a title'),
+          ('Mr', 'Mr'),
+          ('Mrs', 'Mrs'),
+          ('Ms', 'Ms'),
+          ('Miss', 'Miss')]
 
 
 class PasswordReset(FlaskForm):
     email_address = StringField('Email Address', validators=[DataRequired(), Email()])
-    salutation = StringField('Salutation', validators=[DataRequired(), Length(min=2, max=4)])
-    first_name = StringField('First Name', validators=[DataRequired()])
-    surname = StringField('Surname', validators=[DataRequired()])
+    title = SelectField('Title', choices=TITLES, validators=[DataRequired()])
+    forename = StringField('First Name', validators=[DataRequired()])
+    surname = StringField('Last Name', validators=[DataRequired()])
     password = PasswordField('New Password', validators=[DataRequired(),
                              EqualTo('confirm', message='Passwords must match')])
-    confirm = PasswordField('Confirm     Password', validators=[DataRequired()])
+    confirm = PasswordField('Confirm Password', validators=[DataRequired()])
